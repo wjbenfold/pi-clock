62101fe48bcfbcac2ba8b7502cfaf49418e5e765
wjbenfold <will.benfold@bemabe.com>
2024-12-05 18:37:26 +0000

    Sleep to give time for waking before music

--- src/backend/music.py
+++ src/backend/music.py
@@ -1,8 +1,10 @@
 import subprocess
+import time
 
 
 def shoop():
     subprocess.call(["setterm", "--blank", "poke", "> /dev/tty"])
+    time.sleep(1)
     omx_instructions = [
         "mpg123",
         "/home/pi/Music/Shoop.mp3",
